8393be8b43b3fdcc908ed2f93127f5a30d33f189
martin <martin.queval04@gmail.com>
2026-07-06 16:57:38 +0200

    SCRUM-299 : footer legal budgy (infoHref) + pin @custhome/ui v0.7.0
    - BudgyLayout : lien info legal via infoHref={cguUrl()}
    - LegalLinks
    - tests AC (legalFooter scrum299, nav) + fix typage messages callback

--- src/components/LegalLinks.tsx
+++ src/components/LegalLinks.tsx
@@ -1,4 +1,4 @@
-import { Link, Stack, useTranslation } from "@custhome/ui";
+import { Divider, Link, Stack, useTranslation } from "@custhome/ui";
 import { cguUrl } from "../lib/auth-redirect";
 
 const LEGAL_NOTICE_ANCHOR = "mentions-legales";
@@ -7,10 +7,17 @@ export default function LegalLinks() {
   const { t } = useTranslation();
 
   return (
-    <Stack as="nav" gap="xs" alignItems="center" label={t("budgy.legal.footerLabel")}>
+    <Stack
+      as="nav"
+      direction="row"
+      gap="xs"
+      alignItems="center"
+      label={t("budgy.legal.footerLabel")}
+    >
       <Link href={cguUrl()} size="small" color="secondary">
         {t("budgy.legal.cgu")}
       </Link>
+      <Divider orientation="vertical" flexItem spacing="xs" />
       <Link href={cguUrl(LEGAL_NOTICE_ANCHOR)} size="small" color="secondary">
         {t("budgy.legal.notice")}
       </Link>
